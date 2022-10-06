c3aa510e24012e63af69dd851e19622854445e49
JoceVera <111259494+JoceVera@users.noreply.github.com>
2022-10-06 07:59:58 -0500

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,19 +1,18 @@
 from datetime import date
 
-def inicio_01 (lista):
+
+def inicio_01 (matriz):
     """Pide toda la información del pedido"""
     print ("\n      Información del pedido...\n")
     nombre_pedido = str(input("Ingresa el nombre del pedido: "))
-    lista.append(nombre_pedido)
     seccion = str(input("Escribe la letra correspondiente a"\
                         " la sección de producción:\n"\
                         "        A   B   C   D   E   \n"))
     personal = secciones (seccion)
-    piezas_total,piezas_dia= datos (lista)
+    piezas_total,piezas_dia= datos ()
     promedio_produccion_dia, tiempo_restante,faltantes = \
                          rendimiento(piezas_dia,piezas_total,personal,horas_de_trabajo)
-    lista.append (promedio_produccion_dia)
-    resultados_restantes (tiempo_restante,faltantes,lista)
+    matriz.append(crear_lista(nombre_pedido,personal,piezas_dia,promedio_produccion_dia, tiempo_restante,faltantes))
 
     inicio = str(input("\n\n¿Desea agregar otro proceso?\n  Ingrese"\
                        " la opción correpondiente\n         1.Si  2.No\n"))
@@ -43,7 +42,7 @@ def secciones (letra_seccion):
             letra_seccion = str(input("Sección no válida...Escribe la letra"\
                                       " correspondiente a la sección"\
                                     "de producción:\n"))           
-def datos (lista):
+def datos ():
     """Validación de los datos iniciales de la producción:
 la producción del día no puede ser mayor a la total"""
     pedido_total = int(input("Cantidad total de piezas en pedido: "))
@@ -57,7 +56,6 @@ la producción del día no puede ser mayor a la total"""
             produccion_dia = int(input("Cantidad de piezas terminados: "))
         else:
             band=0
-    lista.append(produccion_dia)
     return pedido_total, produccion_dia
 
 def rendimiento (piezas_dia, piezas_pedido, num_personal,horas_de_trabajo):
@@ -67,35 +65,45 @@ def rendimiento (piezas_dia, piezas_pedido, num_personal,horas_de_trabajo):
     estimacion = (piezas_restantes*horas_de_trabajo)/piezas_dia
     return promedio_persona,estimacion,piezas_restantes
 
-def resultados_restantes (estimacion,piezas_restantes,lista):
-    """Mensajes sobre el tiempo de producción y piezas  restante por pedido"""
-    print("\n\nAproximadamente faltan:\n %.2f" %estimacion,"horas de trabajo y",\
-          int(piezas_restantes),"piezas para completar el pedido.")
+def crear_lista (pedido,cantidad_personas,produccion,promedio,tiempo,resto):
+    """Crea una lista por cada pedido con su respectiva información"""
+    lista=[]
+    lista.append(pedido)
+    lista.append(cantidad_personas)
+    lista.append(produccion)
+    lista.append(promedio)
+    lista.append(tiempo)
+    lista.append(resto)
+    return lista
 
-def resumen_dia (lista):
+def resumen_dia (matriz):
     """Genera el resumen de los procesos del día haciendo uso de una lista"""
-    contador=1
     nombre= 0
-    produccion= 1
-    rendimiento_dia= 2
-    if (len (lista))== 0:
-        print ("No hubo pedidos.")
+    produccion= 2
+    rendimiento_dia= 3
+    estimacion=4
+    piezas_restantes=5
+    if (len (matriz))== 0:
+        print ("Pedidos:\n   No hubo pedidos.")
     else:
-        print("Pedidos:")
-        while contador <=(len (lista)/3):
-            print("  El pedido",lista[nombre],"realizó",lista[produccion],"piezas"\
-              " con un rendimiento de %.2f" % lista[rendimiento_dia],"piezas"\
-              " por persona.")
-            nombre+=3
-            produccion+=3
-            rendimiento_dia+=3
-            contador+=1
+        print("Pedidos:\n")
+        indice_01 = 0
+        while indice_01 < len(matriz):
+            indice=0
+            while indice < 1:
+                print(" \n   El pedido",matriz[indice_01][nombre],"realizó",matriz[indice_01][produccion],\
+                      "piezas con un rendimiento de %.2f" % matriz[indice_01][rendimiento_dia],"piezas"\
+                      " por persona.Aproximadamente faltan %.2f" % matriz[indice_01][estimacion],\
+                      "horas de trabajo y", int(matriz[indice_01][piezas_restantes]),\
+                                                  "piezas para completar el pedido.")
+                indice+=1
+            indice_01+=1
     
 
 """Variables y datos reutilizables durante todo el proceso"""
 hoy=date.today()
 cantidad_diaria= 0   
-resumen=[]
+informacion_total=[]
 horas_de_trabajo=8
 
 """Programación principal"""
@@ -109,11 +117,11 @@ band=1
 while band:
     while inicio =="1":
         band=0
-        inicio,cantidad_dia=inicio_01 (resumen)
+        filas_matriz=0
+        inicio,cantidad_dia=inicio_01 (informacion_total)
         cantidad_diaria += cantidad_dia
     if inicio == "2":
         band=0
-        print("No hay más procesos por hoy...")
     else:
         band=1
         inicio = input("\nRespuesta no válida..."\
@@ -121,9 +129,9 @@ while band:
                           " número correpondiente \n          1.Si  2.No\n")
 
 print("\n\n      Resumen del",hoy,"...\n")
-resumen_dia(resumen)
+resumen_dia(informacion_total)
 
 productividad=cantidad_diaria/horas_de_trabajo
-print("\nGeneral:\n El total de piezas"\
+print("\n\nGeneral:\n El total de piezas"\
       " fabricadas fue de", cantidad_diaria," con una productividad total "\
-      "de %.2f" % productividad,"piezas.\n\n\nRegresa pronto.")
+      "de %.0f" % productividad,"piezas.\n\n\nRegresa pronto.")
